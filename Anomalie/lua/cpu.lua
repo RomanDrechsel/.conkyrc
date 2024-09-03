30f3867b8b7462f3555dbc3f10aab24019d959fc
Roman Drechsel <email@roman-drechsel.de>
2024-09-03 17:21:11 +0200

    Delete Anomalie directory

--- Anomalie/lua/cpu.lua
+++
@@ -1,9 +0,0 @@
-function getCpuTemp()
-    if Sensors and Sensors.Json then
-        local temp = Sensors.Json["coretemp-isa-0000"]["Package id 0"]["temp1_input"];
-        if temp then
-            return toInt(temp) .. "°C"
-        end
-    end
-    return ""
-end
